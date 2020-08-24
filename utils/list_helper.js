e4ab1f745de112d3e924a8327850d3a280f1ef4a
Oskar <gustosk@gmail.com>
2020-08-24 15:26:47 +0300

    tehtävä 4.5
    tehtävä 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,30 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (total, currentValue) => total + currentValue
+    if(blogs.length === 0){
+        return 0
+    } else if(blogs.length === 1){
+        return blogs[0].likes
+    } else {        
+        return blogs.map(blog => blog.likes).reduce(reducer)
+    }
+}
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0){
+        return 0
+    } else if(blogs.length === 1){
+        return blogs[0]
+    } else {
+        return blogs.reduce((prev, current) => (prev.likes > current.likes)?prev:current)
+    }
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+dummy,
+totalLikes,
+favoriteBlog
+}
