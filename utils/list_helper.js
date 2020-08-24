dc9162f963edc775cd885bcec255e92651c867d0
Oskar <gustosk@gmail.com>
2020-08-24 19:05:21 +0300

    Tehtävä 4.6
    Tehtävä 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -22,9 +24,34 @@ const favoriteBlog = (blogs) => {
         return blogs.reduce((prev, current) => (prev.likes > current.likes)?prev:current)
     }
 }
+
+const mostBlogs = (blogs) => {
+    if(blogs.length === 0){
+        return 0
+    } else if(blogs.length === 1){
+        const newBlog = {
+            author: blogs[0].author,
+            blogs: 1
+        }    
+        return newBlog
+    } else {
+        const authors = blogs.map(blog => blog.author)
+        const uniqueAuthorsSet = new Set(authors)
+        const uniqueAuthors = [...uniqueAuthorsSet]
+        const authorsBlogCount = []
+        uniqueAuthors.forEach(author => {
+            const blogCount = authors.filter(x => x === author).length       
+            const authorToAdd = [{author: author, blogs: blogCount}]
+            authorsBlogCount.push(authorToAdd)
+        })
+        return authorsBlogCount.reduce((prev, current) => (prev.blogs > current.blogs)?prev:current)
+    }
+}
+
   
 module.exports = {
 dummy,
 totalLikes,
-favoriteBlog
+favoriteBlog,
+mostBlogs
 }
